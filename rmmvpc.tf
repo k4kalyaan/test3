5fda89c1a9b5982adbfd237e1ce96e9e2b822e05
k4kalyaan <60605651+k4kalyaan@users.noreply.github.com>
2021-09-17 15:17:24 +0530

    updated the auto

--- rmmvpc.tf
+++ rmmvpc.tf
@@ -5,7 +5,7 @@ variable "TF_VERSION" {
 }
 
 variable "image_url" {
-  default = "cos://us-south/cos-davidng-south/rmmv4RMMGoldenTemplate.qcow2"
+  default = "cos://us-south/cos-davidng-south/RMMAutoprRackwareIBMGen2AP.qcow2"
 }
 
 /**
